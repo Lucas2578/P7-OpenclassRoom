8db6957847477eccbc338432da93caf7c0b4687a
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-04-10 19:27:39 +0200

    🎉 Add app.js, server.js and packages

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Connecte l'application à la base de données MongoDB
+mongoose.connect('mongodb+srv://Lucas:Rda6ZDgUlrvgu7kC@cluster0.g4bmxef.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+// Middleware pour comprendre les données JSON envoyées dans les requêtes
+app.use(express.json());
+
+// Middleware express qui définit des en-têtes CORS pour permettre les requêtes HTTP entre différentes origines
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+module.exports = app;
