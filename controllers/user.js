afb7cc2cd2f51c198b57c87cb0c122b13723d4a0
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-04-30 22:41:54 +0200

    Add regex for mail verification and add verification password

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,18 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
+    // On initialise une constante qui correspond à une adresse e-mail (regex)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // On retourne un message d'erreur dans le cas où l'adresse mail n'est pas valide
+    if (!emailRegex.test(req.body.email)) {
+        return res.status(400).json({ message: 'L\'adresse e-mail n\'est pas valide !' });
+    }
+
+    // On effectue une vérification qu'un mot de passe est bien indiqué avec 8 caractères minimum
+    if (!req.body.password || req.body.password.length < 8) {
+        return res.status(400).json({ message: 'Le mot de passe doit contenir au moins 8 caractères !' });
+    }
+
     // On appelle la fonction pour hacher le MDP (ici on fait 10 tours de cryptage)
     bcrypt.hash(req.body.password, 10)
         // Promesse qui créer un utilisateur si réussie
